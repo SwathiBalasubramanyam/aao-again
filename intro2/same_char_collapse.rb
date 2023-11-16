4bbd96806df232f380e0b2682c0370c448f98be0
swathi <swathibalasubramanyam@gmail.com>
2023-11-15 16:02:44 -0800

    same char collapse

--- intro2/same_char_collapse.rb
+++ intro2/same_char_collapse.rb
@@ -1,5 +1,19 @@
 def same_char_collapse(str)
 
+    can_we_reduce = true
+
+    while can_we_reduce
+        can_we_reduce = false
+        str.each_char.with_index{|char, idx|
+            if char == str[idx+1]
+                str = str[0...idx] + str[idx+2..-1]
+                can_we_reduce = true
+                break
+            end
+        }
+    end
+
+    str
 end
 
 puts same_char_collapse("zzzxaaxy")   #=> "zy"
