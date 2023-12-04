28c3e85f55c0abf1ac582ad8d83bc6839b429710
swathi <swathibalasubramanyam@gmail.com>
2023-12-03 20:59:04 -0800

    w1d3

---
+++ W1D3/practice-for-ch-foundations-nauseating-numbers-main/part1.rb
@@ -0,0 +1,71 @@
+def strange_sums(arr)
+    count = 0
+    (0...arr.length).each{|i|
+        (i+1...arr.length).each{|j|
+            count += 1 if arr[i] + arr[j] == 0 
+        }
+    }
+    count
+end
+
+# p strange_sums([2, -3, 3, 4, -2])     # 2
+# p strange_sums([42, 3, -1, -42])      # 1
+# p strange_sums([-5, 5])               # 1
+# p strange_sums([19, 6, -3, -20])      # 0
+# p strange_sums([9])                   # 0
+
+def pair_product(arr, prod)
+    (0...arr.length).each {|idx|
+        (idx+1...arr.length).each {|jdx|
+            if arr[idx] * arr[jdx] == prod
+                return true
+            end
+        }
+    }
+    return false
+end
+
+# p pair_product([4, 2, 5, 8], 16)    # true
+# p pair_product([8, 1, 9, 3], 8)     # true
+# p pair_product([3, 4], 12)          # true
+# p pair_product([3, 4, 6, 2, 5], 12) # true
+# p pair_product([4, 2, 5, 7], 16)    # false
+# p pair_product([8, 4, 9, 3], 8)     # false
+# p pair_product([3], 12)             # false
+
+def rampant_repeats(str, hash)
+    new_str = ""
+
+    str.each_char {|char|
+        if hash.keys.include?(char)
+            new_str += char * hash[char]
+        else 
+            new_str += char 
+        end
+    }
+    new_str
+end
+
+# p rampant_repeats('taco', {'a'=>3, 'c'=>2})             # 'taaacco'
+# p rampant_repeats('feverish', {'e'=>2, 'f'=>4, 's'=>3}) # 'ffffeeveerisssh'
+# p rampant_repeats('misispi', {'s'=>2, 'p'=>2})          # 'mississppi'
+# p rampant_repeats('faarm', {'e'=>3, 'a'=>2})            # 'faaaarm'
+
+def perfect_square(num)
+    (1..num).each {|idx|
+        if idx * idx == num
+            return true
+        end
+    }
+    return false
+end
+
+# p perfect_square(1)     # true
+# p perfect_square(4)     # true
+# p perfect_square(64)    # true
+# p perfect_square(100)   # true
+# p perfect_square(169)   # true
+# p perfect_square(2)     # false
+# p perfect_square(40)    # false
+# p perfect_square(32)    # false
+# p perfect_square(50)    # false
---
+++ W1D3/practice-for-ch-foundations-nauseating-numbers-main/part2.rb
@@ -0,0 +1,3 @@
+
+def anti_prime?()
+end
