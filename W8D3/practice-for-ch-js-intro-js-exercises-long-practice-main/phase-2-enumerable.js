4e7a5e2dadfcb6a83b1b4c92d5f76f57ca6165f2
swathi <swathibalasubramanyam@gmail.com>
2024-02-12 18:27:18 -0800

    JS fund pahse 2

--- W8D3/practice-for-ch-js-intro-js-exercises-long-practice-main/phase-2-enumerable.js
+++ W8D3/practice-for-ch-js-intro-js-exercises-long-practice-main/phase-2-enumerable.js
@@ -1,1 +1,36 @@
 // Your code here
+
+Array.prototype.myEach = function(callback){
+
+    for(let idx=0; idx<this.length; idx++){
+        callback(this[idx])
+    }
+}
+
+Array.prototype.myMap = function(callback){
+    let newArr = [];
+    for(let idx=0; idx<this.length; idx++){
+        newArr.push(callback(this[idx]))
+    }
+}
+
+Array.prototype.myReduce = function(callback, initialValue){
+    let accumulator;
+    let arr = this;
+
+    if (initialValue){
+        accumulator = initialValue;
+    } else {
+        accumulator = this[0];
+        arr = this.slice(1);
+    }
+
+    for(let idx=0; idx<arr.length; idx++){
+        accumulator = callback(accumulator, arr[idx]);
+    }
+    return accumulator;
+}
+
+let arr = [1,2,3,4];
+
+console.log(arr.myReduce((acc, el) => acc + el, 5))
